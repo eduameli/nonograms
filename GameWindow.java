f6d39165528f1e51d7a53c9f9c4bdbcf05977108
eduameli <eduardohernandezmelian@gmail.com>
2024-04-26 01:20:34 +0200

    made info tiles part of the nonogram grid!

--- GameWindow.java
+++ GameWindow.java
@@ -18,19 +18,11 @@ public class GameWindow {
 
         innerLayout = new GridLayout(rows+1, columns+1);
         innerPanel.setLayout(innerLayout);
-
         populateNonogram();
 
         window.setTitle("nonograms");
 
-        //GridLayout innerPanel = new GridLayout();
-
-        //JPanel innerPanel = new JPanel();
         innerPanel.setLayout(innerLayout);
-
-
-
-        // Set preferred width for leftInfo panel
 
        
 
@@ -40,48 +32,25 @@ public class GameWindow {
     }
 
     public void populateNonogram() {
-        //for (int i = 0; i < rows+1*columns+1; i++) {
-            //if 
-        //    PlayableTile tile = new PlayableTile();
-        //    tile.setPreferredSize(new Dimension(64, 64));
-            //tile.setBackground(Color.yellow);
-        //    innerPanel.add(tile);
-        //}
+
 
         for(int y = 0; y < columns+1; y++) {
             for(int x = 0; x < rows+1; x++) {
                
-                //if(x == 0 && y == 0 ){
-                //    continue;
-                //}
-                
-               
                 if (y == 0) {
                     //JLabel info = new JLabel("<html>First Line<br>Second Line<br>Second Line<br>Second Line<br>Second Line</html>");
-                    GridLayout infoLayout = new GridLayout(5, 1);
-                    JPanel infoPan =new JPanel();
-                    infoPan.setLayout(infoLayout);
-                    infoPan.add(new JLabel("1"));
-                    infoPan.add(new JLabel("2"));
-                    infoPan.add(new JLabel("3"));
-                    infoPan.add(new JLabel("4"));
-                    infoPan.add(new JLabel("5"));
+                    //GridLayout infoLayout = new GridLayout(5, 1);
+                    innerPanel.add(new InfoTile());
 
-                    innerPanel.add(infoPan);
+                    //innerPanel.add(infoPan);
                 } else if(x == 0) {
-                    GridLayout infoLayout = new GridLayout(1, 5);
-                    JPanel infoPan =new JPanel();
-                    infoPan.setLayout(infoLayout);
-                    infoPan.add(new JLabel("1"));
-                    infoPan.add(new JLabel("2"));
-                    infoPan.add(new JLabel("3"));
-                    infoPan.add(new JLabel("4"));
-                    infoPan.add(new JLabel("5"));
+
+
                     
-                    innerPanel.add(infoPan);
+                    innerPanel.add(new InfoTile());
 
                 } else {
-                    PlayableTile tile = new PlayableTile("(" + x + ", " + y + ")");
+                    PlayableTile tile = new PlayableTile();
                     tile.setPreferredSize(new Dimension(64, 64));
                     innerPanel.add(tile);
                 }
---
+++ InfoTile.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class InfoTile extends JPanel {
+    public InfoTile() {
+        this.add(new JLabel("1"));
+        this.add(new JLabel("1"));
+
+        this.add(new JLabel("1"));
+
+        this.add(new JLabel("1"));
+
+        this.add(new JLabel("1"));
+
+        this.add(new JLabel("1"));
+
+    }
+}
